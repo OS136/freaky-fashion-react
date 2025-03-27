0211ddcc90074b82b027034cfc68c5a787c33752
OS136 <olenashereiko@gmail.com>
2025-03-27 14:52:50 +0100

    Refactor DeliveryBadge component layout for improved responsiveness and styling

--- client/src/components/Footer/DeliveryBadge.jsx
+++ client/src/components/Footer/DeliveryBadge.jsx
@@ -1,53 +1,49 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faEarthAfrica,
   faJetFighter,
   faShieldHalved,
 } from "@fortawesome/free-solid-svg-icons";
 import { faFaceSmile } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const DeliveryBadges = () => {
   return (
-    <div className="flex flex-col sm:flex-row sm:flex-wrap sm:justify-evenly lg:flex-nowrap gap-5 mx-2">
-      {/* First Item */}
-      <a className="flex sm:flex-row sm:items-center sm:w-[47%] lg:w-auto w-full">
-        <div className="flex-none">
-          <FontAwesomeIcon icon={faEarthAfrica} className="w-12 h-7 mr-3" />
-        </div>
-        <span className="whitespace-nowrap flex-[1_1_80%] p-5">
-          Gratis frakt och returer
-        </span>
-      </a>
+    <div className="flex flex-col mx-4 mb-4 sm:flex-row sm:flex-wrap sm:justify-left md:justify-evenly md:gap-5 lg:flex-nowrap">
+      <div className="grid md:grid-flow-col md:grid-rows-2  md:gap-x-24 lg:grid-rows-1 lg:gap-x-0 lg:w-full lg:place-items-center">
+        {/* First Item */}
+        <a className="flex items-center sm:flex-row sm:items-center sm:w-[246px]">
+          <div className="flex-none">
+            <FontAwesomeIcon icon={faEarthAfrica} className="w-12 h-7 mr-3" />
+          </div>
+          <span className="whitespace-nowrap p-5">
+            Gratis frakt och returer
+          </span>
+        </a>
 
-      {/* Second Item */}
-      <a className="flex sm:flex-row sm:items-center sm:w-[47%] lg:w-auto w-full">
-        <div className="flex-none">
-          <FontAwesomeIcon icon={faJetFighter} className="w-12 h-7 mr-3" />
-        </div>
-        <span className="whitespace-nowrap flex-[1_1_80%] p-5">
-          Express frakt
-        </span>
-      </a>
+        {/* Second Item */}
+        <a className="flex items-center sm:flex-row sm:items-center sm:w-[246px]">
+          <div className="flex-none">
+            <FontAwesomeIcon icon={faJetFighter} className="w-12 h-7 mr-3" />
+          </div>
+          <span className="whitespace-nowrap p-5">Express frakt</span>
+        </a>
 
-      {/* Third Item */}
-      <a className="flex sm:flex-row sm:items-center sm:w-[47%] lg:w-auto w-full">
-        <div className="flex-none">
-          <FontAwesomeIcon icon={faShieldHalved} className="w-12 h-7 mr-3" />
-        </div>
-        <span className="whitespace-nowrap flex-[1_1_80%] p-5">
-          Säkra betalningar
-        </span>
-      </a>
+        {/* Third Item */}
+        <a className="flex items-center sm:flex-row sm:items-center sm:w-[246px]">
+          <div className="flex-none">
+            <FontAwesomeIcon icon={faShieldHalved} className="w-12 h-7 mr-3" />
+          </div>
+          <span className="whitespace-nowrap p-5">Säkra betalningar</span>
+        </a>
 
-      {/* Fourth Item */}
-      <a className="flex sm:flex-row sm:items-center sm:w-[47%] lg:w-auto w-full">
-        <div className="flex-none">
-          <FontAwesomeIcon icon={faFaceSmile} className="w-12 h-7 mr-3" />
-        </div>
-        <span className="whitespace-nowrap flex-[1_1_80%] p-5">
-          Nyheter varje dag
-        </span>
-      </a>
+        {/* Fourth Item */}
+        <a className="flex items-center sm:flex-row sm:items-center sm:w-[246px]">
+          <div className="flex-none">
+            <FontAwesomeIcon icon={faFaceSmile} className="w-12 h-7 mr-3" />
+          </div>
+          <span className="whitespace-nowrap p-5">Nyheter varje dag</span>
+        </a>
+      </div>
     </div>
   );
 };
